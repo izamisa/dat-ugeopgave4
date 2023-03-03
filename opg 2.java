325a5e022deeb2d6c7b2c5b883ec08b8c2653f32
izamisa <124897143+izamisa@users.noreply.github.com>
2023-03-03 01:11:38 +0100

    Add files via upload

---
+++ opg 2.java
@@ -0,0 +1,12 @@
+
+
+public class MyMainClass {
+    public static void main(String[] args) {
+        System.out.println("Please type your name");
+        java.util.Scanner stringScanner = new java.util.Scanner(System.in);
+        String name = stringScanner.next
+        System.out.println("Please type your age");
+        stringScanner close
+        )
+    }
+}
